2e045cd6f60a3a6c5fffbcd89612bc016e777167
Mikuhann <ng.kano@gmail.com>
2019-07-10 20:01:41 +0300

    basic route setup

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.get('/', (req, res) => res.json({msg: 'Address book API'}));
 
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
