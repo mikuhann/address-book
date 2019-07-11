b64dd41341a3b7d10221c309bb13c09d27eb55bf
Mikuhann <ng.kano@gmail.com>
2019-07-11 20:58:57 +0300

    add update contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -11,7 +11,7 @@ const Contact = require('../models/Contact');
 // @desc    Get all users contacts
 // @access  Private
 
-router.get('/', authMiddleware, asyncMiddleware(async (req, res) => {
+router.get('/', authMiddleware, asyncMiddleware(async (req, res, next) => {
   const contacts = await Contact.find({user: req.user.id}).sort({date: -1});
   res.json(contacts);
 }));
@@ -23,7 +23,7 @@ router.get('/', authMiddleware, asyncMiddleware(async (req, res) => {
 router.post('/', [authMiddleware, [
   check('name', 'Name field is required').not().isEmpty(),
   check('email', 'Email field is required').not().isEmpty()
-]] , asyncMiddleware(async (req, res) => {
+]] , asyncMiddleware(async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({errors: errors.array()});
@@ -44,9 +44,31 @@ router.post('/', [authMiddleware, [
 // @desc    update contact
 // @access  Private
 
-router.put('/:id', (req, res) => {
-  res.send('Update contact');
-});
+router.put('/:id', authMiddleware, asyncMiddleware(async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
+  const {name, email, phone, type} = req.body;
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+  let contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    return res.status(400).json({msg: 'Contact not found'});
+  }
+  if (contact.user.toString() !== req.user.id) {
+    return res.status(401).json({msg: 'Not authorized'});
+  }
+  contact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    {$set: contactFields},
+    {new: true}
+  );
+  res.json(contact);
+}));
 
 // @route   DELETE /api/contacts/:id
 // @desc    delete contact
