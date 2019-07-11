fd5db1a08cf543014c143c60752fc024606f9bb9
Mikuhann <ng.kano@gmail.com>
2019-07-11 19:53:57 +0300

    add auth middleware add auth for user add protected route

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const {check, validationResult} = require('express-validator');
+const asyncMiddleware = require('../utils/asyncMiddleware');
+const authMiddleware = require('../utils/authMiddleware');
+
+const User = require('../models/User');
 
 // @route   GET /api/auth
 // @desc    Get logged in user
 // @access  Private
 
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
-});
+router.get('/', authMiddleware, asyncMiddleware(async (req, res) => {
+  res.send('private route');
+}));
 
 // @route   POST /api/auth
 // @desc    Auth user and get token back
 // @access  Public
 
-router.post('/', (req, res) => {
-  res.send('Login user')
-});
+router.post('/', [
+  check('email', 'Please, input a valid email').isEmail(),
+  check('password', 'Please, input password').exists()
+], asyncMiddleware(async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  }
+  const {email, password} = req.body;
+  let user = await User.findOne({email});
+  if (!user) {
+    return res.status(400).json({msg: 'Invalid credentials'});
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({msg: 'Invalid credentials'});
+  }
+  const payload = {
+    user: {
+      id: user.id
+    }
+  };
+  const jwtSecret = config.get('jwtSecret');
+  const jwtOptions = {
+    expiresIn: 3600
+  };
+  jwt.sign(payload, jwtSecret, jwtOptions, (err, token) => {
+    if (err) throw err;
+    res.json({token});
+  })
+}));
 
 module.exports = router;
