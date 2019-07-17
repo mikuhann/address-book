0affc1f4f710fb3172c7a2e3dadbf46796cb48c2
Mikuhann <ng.kano@gmail.com>
2019-07-17 19:05:17 +0300

    add css animation to contact component

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -13,11 +13,23 @@ const Contacts = () => {
   }
   return (
     <Fragment>
-      {filter ? filter.map((contact) => (
-        <ContactItem key={contact.id} contact={contact}/>
-      )) : contacts.map((contact) => (
-        <ContactItem key={contact.id} contact={contact}/>
-      ))}
+      <TransitionGroup>
+        {filter ? filter.map((contact) => (
+          <CSSTransition
+            key={contact.id}
+            timeout={500}
+            classNames="item">
+            <ContactItem contact={contact}/>
+          </CSSTransition>
+        )) : contacts.map((contact) => (
+          <CSSTransition
+            key={contact.id}
+            timeout={500}
+            classNames="item">
+            <ContactItem contact={contact}/>
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
