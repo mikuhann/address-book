8b28d0992479e88d8a0e6ccf691268933982f23e
Mikuhann <ng.kano@gmail.com>
2019-07-22 17:25:38 +0300

    preparing to deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -8,11 +9,15 @@ connectDB();
 // body parser
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({msg: 'Address book API'}));
-
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// production config
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build')));
+}
 
 const PORT = process.env.PORT || 5000;
 
