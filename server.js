21b3e76573bd8d1275fe48733540554f350f61f7
Mikuhann <ng.kano@gmail.com>
2019-07-22 20:40:44 +0300

    fix

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/api/contacts', require('./routes/contacts'));
 // production config
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
